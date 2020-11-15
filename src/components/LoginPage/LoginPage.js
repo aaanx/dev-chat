1e9e6c6fd2f0391f70cd007fccb195f17df67dd7
MadFrankx <magdalena.frankowska93@gmail.com>
2020-11-15 14:27:30 +0100

    ADD basic logic to login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,7 +1,33 @@
 import React from "react";
-import { Button, Input, Form } from "semantic-ui-react";
+import { Button, Input, Form, Container } from "semantic-ui-react";
 
 class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginChange = this.handleLoginChange.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.state = {
+      loginData: {
+        username: "",
+        password: "",
+      },
+    };
+  }
+
+  handleLoginChange = (e, { value, name }) => {
+    this.setState({
+      formData: {
+        [name]: value,
+      },
+    });
+  };
+
+  handleLoginSubmit = () => {
+    const { username, password } = this.state.loginData;
+    this.setState({ formData: { username: username, password: password } });
+    //send to firebase
+  };
+
   render() {
     const LoginWrapperStyle = {
       margin: "5rem auto",
@@ -10,22 +36,35 @@ class LoginPage extends React.Component {
 
     return (
       <div className="login-wrapper" style={LoginWrapperStyle}>
-        <Form>
-          <Form.Group widths={2}>
-            <Form.Field>
-              <label>Username or email</label>
-              <Input name="username" placeholder="Username or email" />
-            </Form.Field>
-            <Form.Field>
-              <label>Password</label>
-              <Input type="password" name="password" placeholder="Password" />
-            </Form.Field>
-          </Form.Group>
+        <Container>
+          <h1> Log In </h1>
+          <Form onSubmit={this.handleLoginSubmit}>
+            <Form.Group widths={2}>
+              <Form.Field required>
+                <label>Username or email</label>
+                <Input
+                  type="text"
+                  name="username"
+                  placeholder="Username or email"
+                  onChange={this.handleLoginChange}
+                />
+              </Form.Field>
+              <Form.Field required>
+                <label>Password</label>
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleLoginChange}
+                />
+              </Form.Field>
+            </Form.Group>
 
-          <Button primary type="submit">
-            Login
-          </Button>
-        </Form>
+            <Button primary type="submit">
+              Login
+            </Button>
+          </Form>
+        </Container>
       </div>
     );
   }
